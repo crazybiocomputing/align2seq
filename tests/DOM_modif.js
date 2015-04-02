9c52495f1d54596b6bc627d150048b4bd9644ede
Aurelien Beliard <abeliard@emi.u-bordeaux1.fr>
2015-04-02 18:36:40 +0200

    message

--- tests/DOM_modif.js
+++ tests/DOM_modif.js
@@ -1,15 +1,33 @@
-	function interface(){
+
+"use strict"
+
+// function interface(){
+var	validate=function(){
+	 	if (document.getElementById('proteine').checked==true){
+	 		document.getElementById('nucleotide').checked=false;
+	 	}
+	 	else if (document.getElementById('nucleotide').checked==true){
+	 		document.getElementById('proteine').checked=false;
+
+	 	}
+	 	else if ((document.getElementById('nucleotide').checked==true)&&(document.getElementById('proteine').checked==true){
+	 		var form=document.querySelector("form");
+	 		form p.innerHtml ="vous devez cocher au moins une des deux case"
+
+	 	}
 
 
+	};
 
-	this.choix=function(liste) {
-		if 
-
-	}
+	this.action=function(){
 
 
+	};
 
+	
+	this.form=function(){
+		
 
+	};
 
-
-}
+	
