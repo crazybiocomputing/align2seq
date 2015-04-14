32059286750f34cfe6deca09717e4a8c7fde903f
Aurore Perdriau <aperdriau65n@emi.u-bordeaux1.fr>
2015-04-14 18:06:36 +0200

    Matrice de score

--- src/DOM_modif.js
+++ src/DOM_modif.js
@@ -1,122 +1,246 @@
-"use strict"
-//useless function
-
-// function validate(){
-// 	 	if (document.getElementById('proteine').checked===true){
-// 	 		document.getElementById('nucleotide').checked=false;
-// 	 	}
-// 	 	else if (document.getElementById('nucleotide').checked===true){
-// 	 		document.getElementById('proteine').checked=false;
-
-// 	 	}
-// 	 	else if ((document.getElementById('nucleotide').checked===false)&&(document.getElementById('proteine').checked===false)){
-// 	 		var form=document.querySelector("#form p");
-// 	 		form.innerHtml ="vous devez cocher au moins une des deux case";
-
-// 	 	}
-
-
-// 	}
-
-function chooseMatrice(){
-	console.log("AHAHAH");
-			
-		if (document.getElementById('proteine').checked===true){
-			
-			var i=0;
-			var j=30;
-			while  ( i<13){
-				console.log("ahahah");
-				document.forms['form'].choix_matrice.options[i] = new Option('Blosum'+j,'Blosum'+j);
-				i+=1;
-				j+=5;
-				}
-			}
-		
-		else if (document.getElementById('nucleotide').checked===true){
-			console.log("AHahAhah");
-			if (document.forms['form'].choix_matrice.options.length!==0){
-				while (document.forms['form'].choix_matrice.options[1]){
-			 		document.forms['form'].choix_matrice.removeChild(document.forms['form'].choix_matrice.options[0]);
-				}
-				document.forms['form'].choix_matrice.options[0]=new Option('DNAfull','DNAfull');
-			}
-			else{
-				document.forms['form'].choix_matrice.options[0]=new Option('DNAfull','DNAfull');
-			 }
-		}
-		console.log((document.forms['form'].elements.value));
-		}
-
+// var score=require('/score');
+// var chemin=require('./code_nouveau/chemin');
+// var infos = require('./code_nouveau/dom_modif.js');
 
 function show_matrice(){
 	var seq1="ACVHFFTCA"; var seq2="AGFTCDFGA";
 	var s1=seq1.split("");
 	var s2=seq2.split("");
-	var len1= seq1.length;
+	var len1=seq1.length;
 	var len2=seq2.length;
-	var taille1=len1+2;
+	var taille1=len1+1;
 	var taille2=len2+2;
-	// creer un tableau contenant i ligne et j colonne et contenant les seq 
-	console.log(len1);
-	console.log(len2);
-	var matrice=document.getElementById("matrice");
 
-	for (var i =0;i<=taille1;i++){
+	var l1,l2,mat1,mat2;
+	var s1,s2;
+	var matseq,matchemin;
+	var ajout;
+	
+	//On va remplir des listes
+	matseq=[];
+	matchemin=[];
+	matscore1=[];
+	//Longueur minimale entre les deux
+	longueur=Math.min(len1,len2);
+	//Longueur maximale entre les deux
+	maxi=Math.max(len1,len2);
+	matseq[0]="-";
+
+	//Pour elems1 plus petit que la longueur de séquence1, remplir la liste matscore
+	for (var elems1=0;elems1<=len1;elems1++){
+		matseq.push(s1[elems1]);
+	}
+	matseq[len1+1]="-";
+	//Si ajout est différent de 0 
+	//Et longueur minimale est len2
+	if (ajout!==0 && longueur==len2){
+	//Pour indent2 plus petit que ajout, on met un - dans s2
+		for (var indent2=0;indent2<ajout;indent2++){
+			s2.push("-");}}
+	for (var elems2=0;elems2<len2;elems2++){
+		matseq.push(s2[elems2]);
+	}
+
+	var place=0;
+	for(var i=0;i<=len1;i++){
+		for(var j=len1+1;j<=((len1+len2)+1);j++){
+			score(matscore1,matchemin,matseq[i],matseq[j],len2,place);
+			place++;
+		}
+	}
+	var result=alignment(matchemin,matscore1,s1,s2,len1,len2,len2);
+	document.getElementById('matrice').innerHTML +='Alignement 1:'+result[0]+'<br>';
+	document.getElementById('matrice').innerHTML +='Alignement 2:'+result[1]+'<br><br>';
+	document.getElementById('matrice').innerHTML +='Matrice séquences :'+ matseq+'<br>';
+	document.getElementById('matrice').innerHTML +='Matrice scores :'+ matscore1+'<br>';
+	document.getElementById('matrice').innerHTML +='Matrice chemins :'+ matchemin+'<br>';
+
+	
+	var matrice=document.getElementById("matrice2");
+
+	for (var i =0;i<=(taille1+1);i++){
 		matrice.insertRow(i);
 		for(var j=0;j<=taille2;j++){
 			matrice.rows[i].insertCell(j);
 		}
 	}
 	
-	var matrice1=document.getElementById("matrice").rows;//création des lignes
+	var matrice1=document.getElementById("matrice2").rows;//création des lignes
 
 	for (var i = 0 ; i < matrice1.length; i++) {
 
 		var colonne = matrice1[i].cells; //On a autant de cellule par ligne
 		
 		for (var j = 0; j < colonne.length ; j++) {
-			if(j % 2 == 0)//si la clé est paire
-			{
-				colonne[j].style.backgroundColor = "#bdcbf5";
-			}
-			else //elle est impaire
-			{
-				colonne[j].style.backgroundColor = "#829eeb";
-			}
-			if (j===0 && i>=2){ 
-				console.log("on est dans le 1er if");
-				console.log("tour",i)
+			// if(j % 2 == 0)//si la clé est paire
+			// {
+			// 	colonne[j].style.backgroundColor = "#bdcbf5";
+			// }
+			// else //elle est impaire
+			// {
+			// 	colonne[j].style.backgroundColor = "#829eeb";
+			// }
+			if (i>=2 && j===0){ 
+				// console.log("on est dans le 1er if");
+				// console.log("tour",i)
 				for(var colonne in s1){
-					console.log("Colonne : " + colonne + " = " + s1[colonne]);
-					matrice.rows[i].cells[j].innerHTML=s1[colonne];	
-					i++;	
-
-
+					// console.log("Colonne : " + colonne + " = " + s1[colonne]);
+					matrice2.rows[i].cells[j].innerHTML=s1[colonne];
+					i++;		
 				}
-			}
+			};
 
-			if (i===0 && j>=2) { //Remplir la première ligne à partir de la seconde case
-				console.log("on est dans le 2nd if");
-				console.log("tour",j)
+			if (i===0 && j>=3) { //Remplir la première ligne à partir de la seconde case
+				// console.log("on est dans le 2nd if");
+				// console.log("tour",j)
 				for (var ligne in s2) {
-					console.log("Ligne : "+ ligne + " = " + s2[ligne]);
-					//On ressort que la dernière lettre à chaque fois
-					matrice.rows[i].cells[j].innerHTML=s2[ligne];
+					// console.log("Ligne : "+ ligne + " = " + s2[ligne]);
+					matrice2.rows[i].cells[j].innerHTML=s2[ligne];
 					j++;
+
 				}
-			}
+			};
+
+			if (i>=1 && j===2){
+				for (scoring in matscore1){
+					matrice2.rows[i].cells[j].innerHTML=matscore1[scoring];
+					j++;
+					if(j%(taille2+1)==0){
+						j=2;
+						i++;
+					}
+				}
+			i=1;
+			};
 
 		}
-		
 	}
+	
+};
+
+//Calcul des scores des matrices
+function score(matscore1,matchemin,l1,l2,lengthseq,place){
+	var match=10;
+	var mismatch=-5;
+	var gap=2;
+	var currentscore;
+	var scorevert,scorehor,scoredia;
+	var sumvert,sumdia,sumhor;
+	var placevert=place-(lengthseq+1);
+	var placehor=place-1;
+	var placedia=place-(lengthseq+2);
+
+	//Si la place est inférieure ou égale à la longueur de la séquence
+	//Ou que le reste de place/lengthseq+1 est égale à 0 on met tout à 0
+	if((place<=lengthseq) || (place%(lengthseq+1)===0)){
+		matscore1[place]=0;
+		matchemin[place]=0;
+		scorevert=0;
+		scorehor=0;
+		scoredia=0;
+	}
+	//Autrement on calcul les scores
+	else{
+		scorevert=matscore1[placevert];
+		scorehor=matscore1[placehor];
+		scoredia=matscore1[placedia];
+		//Si on a une même lettre alors on a un match soit 10
+		if (l1===l2){
+			currentscore=match;
+		}
+		//Si d'un côté ou de l'autre ou encore des deux côtés on a un vide
+		//C'est une gap soit 2
+		else if (l1==="-" || l2==="-" ||(l1==="-" && l2==="-" )){
+			currentscore=gap;
+		}
+		//Si les lettres sont différentes alors on a un mismatch soit -5
+		else if (l1!=l2){
+			currentscore=mismatch;
+		}
+		//Calcul des trois scores possibles
+		sumdia=scoredia+currentscore;
+		sumvert=scorevert+gap;
+		sumhor=scorehor+gap;
+		//Récupération du score maximal entre les trois
+		var maxiscore=Math.max(sumvert,sumdia,sumhor);
+		//Mettre le score obtenu dans la matrice de score
+		matscore1[place]=maxiscore;
+		//Remplissage de la matrice chemin suivant la matrice score
+		if (maxiscore==(sumhor)){
+			matchemin[place]=1;	} 
+		else if (maxiscore==(sumdia)){
+			matchemin[place]=2;}		
+		else{
+			matchemin[place]=3;	} 
+	}
+}
+
+//Création des alignements
+function alignment(matchemin,matscore1,s1,s2,len1,len2,lengthseq){
+	var val,elem,dep,valmaxpos;
+	var valmax=0;
+	var compt=0;
+	var l=[];
+	var align1=[];
+	var align2=[];
+	//Pour toute valeur dans la matrice de score
+	//valmaxpos est sa place dans la matrice
+	for (val in matscore1){
+		valmaxpos=matscore1[val];
+		valmax=Math.max(valmax,valmaxpos);
+	}
+	//Pour tout élément dans la matrice de score
+	//Si le score correspond à valmax on le met dans l
+	for (elem in matscore1){
+		if (matscore1[elem]===valmax){
+			l.push(elem);
+		}
+	}
+
+	for (dep in l){
+		deppos=l[dep];
+		//99 valeurs 11*9
+		while (true){
+			if (matscore1[deppos]===0){
+				//Unshift ajoutede nouveaux éléments au début de la liste
+				//Si le score est de 0 on ajoute l'élément s1[len1] à align1
+				//Idem pour align2
+				align1.unshift(s1[len1]);
+				align2.unshift(s2[len2]);
+				break;
+			}
+			//Si le score est de 1 on ajoute un - dans align1
+			//On ajoute s2[len2] au début de align2 
+			//len2=longueur de la séquence2 donc on diminue de 1
+			if (matchemin[deppos]===1){
+				deppos=deppos-1;
+				align1.unshift("-");
+				align2.unshift(s2[len2]);
+				len2--;
+			} 
+			//si le score est de 2 on fait de la diagonale
+			//lengthseq = longueur de la séquence soit 9
+			else if (matchemin[deppos]===2){
+				deppos=deppos-(lengthseq+2);
+				align1.unshift(s1[len1]);
+				align2.unshift(s2[len2]);
+				len1--;
+				len2--;
+			}
+			//Autrement (quand score = 3)	
+			//On ajoute s1[len1]au début de align1
+			//On ajoute un - en face dans align2	
+			else{
+				deppos=deppos-(lengthseq+1);
+				align1.unshift(s1[len1]);
+				align2.unshift("-");
+				len1--;
+			} 
+		}
+	}
+	//On met les résultats des alignements dans une variable que l'on retourne
+	var result=[align1,align2];
+	return result;
 
 }
 
-// function step_to_step(){}
-
-//var arrayligne = document.getElementById("matrice").rows;
-//for (var k=0; k<=arrayligne.length;i++){
-
-
-// }
