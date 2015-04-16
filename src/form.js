e841b9b9dc10dd0a958d884ce4f3f64ed74af9e0
EmelineD <emeline.duquenne@etu.u-bordeaux.fr>
2015-04-16 21:41:15 +0200

    Ajout de la licence

--- src/form.js
+++ src/form.js
@@ -1,3 +1,29 @@
+/*
+ *  align2seq: Pairwise alignements algorithms in JavaScript, html5, and css3 
+ *  Copyright (C) 2015  
+ *
+ *  This file is part of align2seq.
+ *
+ *  align2seq is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  align2seq is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with align2seq.  If not, see <http://www.gnu.org/licenses/>
+ *
+ * Authors:
+ * Rudy Anne
+ * Aurelien Beliard
+ * Emeline Duquenne
+ * Aurore Perdriau
+ */
+
 "use strict";
 
 
