feea400642015258d81305832c6e425f0f48a6a2
PerdriauA <perdriau.aurore@gmail.com>
2015-04-30 15:16:45 +0200

    timelapse commenté

--- src/timelapse.js
+++ src/timelapse.js
@@ -23,15 +23,7 @@
  * Emeline Duquenne
  * Aurore Perdriau
  */
-/*
-function timelapse(cpt){
-	// step_score();
-	step_sum(cpt);
-	// step_path(cpt);
-	cpt++;
-	return cpt;
-}
-*/
+
 "use strict"
 
 var nbValuesToDisplay = 0;
@@ -39,7 +31,7 @@ var nbValuesPathToDisplay = 0;
 
 function next(){
 	nbValuesToDisplay++;
-	//vérification des limites des valeurs
+	//Limit check value
 	if(nbValuesToDisplay>matscore.length){
 		nbValuesToDisplay=matscore.length;
 		nbValuesPathToDisplay++;
@@ -66,17 +58,20 @@ function prev(){
 	launch_nstep(nbValuesToDisplay);
 	launch_nstep_path(nbValuesPathToDisplay);
 }
-
+/** Step by step function with next and preview possibilities
+@constructor
+@param {number} nbValuesToDisplay - counter for the scoring matrix 
+*/
 function launch_nstep(nbValuesToDisplay){
 
 	var matrixs=document.getElementById("matrixtime");
 
-	//On vide le tatbleau
+	//The table is empty
 	while (matrixs.firstChild) {
     	matrixs.removeChild(matrixs.firstChild);
 	}
 
-	// on remplit le tableau avec le nombre de cases souhaitées
+	// Filling the array with the desired number of cells
 	for (var i =0;i<=size1;i++){
 		matrixs.insertRow(i);
 		for(var j=0;j<=(size2-1);j++){
@@ -89,12 +84,12 @@ function launch_nstep(nbValuesToDisplay){
 		for(var j=0;j<currentRow.cells.length;j++){
 			var currentCell=currentRow.cells[j];
 
-			//On remplit le tableau pour mettre la première séquence (première colonne)
+			//Filling the array with the first sequence (first column)
 			if (i>=2 && j===0){
 				currentCell.innerHTML=s1[i-2];
 			}
 
-			//On remplit le tableau pour mettre la deuxième séquence (première ligne)
+			//Filling the array with the second sequence (first ligne)
 			if (i===0 && j>=2){
 				currentCell.innerHTML=s2[j-2];
 			}
@@ -107,7 +102,7 @@ function launch_nstep(nbValuesToDisplay){
 		for(var j=1;j<currentRow.cells.length;j++){
 			var currentCell=currentRow.cells[j];
 
-			//On remplit le tableau en partant du principe qu'il est rempli de gauche à droite
+			//The table is filled with the assumption that it is filled from left to right
 			currentCell.innerHTML=matscore[nbDisplayedValues];
 			nbDisplayedValues++;
 
@@ -117,40 +112,13 @@ function launch_nstep(nbValuesToDisplay){
 		}
 	}
 }
-
+/** Step by step function with next and preview possibilities
+@constructor
+@param {number} nbValuesPathToDisplay - counter for path matrix
+*/
 function launch_nstep_path(nbValuesToDisplayPath){
 
 	var matrixs=document.getElementById("matrixtime");
-
-	// //On vide le tatbleau
-	// while (matrixs.firstChild) {
- //    	matrixs.removeChild(matrixs.firstChild);
-	// }
-
-	// // on remplit le tableau avec le nombre de cases souhaitées
-	// for (var i =0;i<=size1;i++){
-	// 	matrixs.insertRow(i);
-	// 	for(var j=0;j<=(size2-1);j++){
-	// 		matrixs.rows[i].insertCell(j);
-	// 	}
-	// }
-
-	// for(var i=0;i<matrixs.rows.length;i++){
-	// 	var currentRow = matrixs.rows[i];
-	// 	for(var j=0;j<currentRow.cells.length;j++){
-	// 		var currentCell=currentRow.cells[j];
-
-	// 		//On remplit le tableau pour mettre la première séquence (première colonne)
-	// 		if (i>=2 && j===0){
-	// 			currentCell.innerHTML=s1[i-2];
-	// 		}
-
-	// 		//On remplit le tableau pour mettre la deuxième séquence (première ligne)
-	// 		if (i===0 && j>=2){
-	// 			currentCell.innerHTML=s2[j-2];
-	// 		}
-	// 	}
-	// }
 
 	var nbDisplayedValuesPath= 0; 
 	for(var i=matrixs.rows.length-1;i>=1;i--){
@@ -162,7 +130,6 @@ function launch_nstep_path(nbValuesToDisplayPath){
 
 			var matPos = currentRowNumber * (currentRow.cells.length-1) + currentCellNumber;
 
-			//On remplit le tableau en partant du principe qu'il est rempli de gauche à droite
 			if (nbDisplayedValuesPath<nbValuesToDisplayPath) {
 				currentCell.innerHTML+=matpath[matPos];
 			};
@@ -170,162 +137,3 @@ function launch_nstep_path(nbValuesToDisplayPath){
 		}
 	}
 }
-/*
-function step_sum(){
-
-	var id=[];
-
-	var matrix1=document.getElementById("matrixtime").rows;
-
-	for (var i = 0 ; i < matrix1.length; i++) {
-
-		var column = matrix1[i].cells; 
-		
-		for (var j = 0; j < column.length ; j++) {
-
-			if (i>=2 && j===0){ 
-				for(var column in s1){
-					matrixtime.rows[i].cells[j].innerHTML=s1[column];	
-					i++;	
-				}
-			}
-
-			if (i===0 && j>=2) {
-				for (var ligne in s2) {
-					matrixtime.rows[i].cells[j].innerHTML=s2[ligne];
-					j++;
-
-				}
-			}
-			
-			
-			if(i>=1 && j===1){
-				
-				for (scoring in matscore){
-					matrixtime.rows[i].cells[j].setAttribute("id",i+":"+j);
-					matrixtime.rows[i].cells[j].style.visibility="hidden";
-					matrixtime.rows[i].cells[j].innerHTML=matscore[scoring];
-					var z=matrixtime.rows[i].cells[j].getAttribute("id",i+":"+j)
-					id.push(z)	
-					j++;
-					if(j%size2==0){
-						i++;
-						j=1;
-
-					}
-				}
-				i=1;
-			}
-		}
-	}	
-	document.getElementById("next").addEventListener("click",pass(id,id[cpt]));	
-	cpt++;
-	console.log(cpt);
-}	
-*/
-
-
-// function pass(id,pos){
-// 	console.log(pos)
-// 	for (var x=0;x<=id.length;x++){
-// 		document.getElementById(pos).style.visibility="visible";
-// 	}
-// 	// if (document.getElementById("10:10")===true){
-// 	// 	step_path();
-// 	// }
-// }
-
-// function step_path(){
-
-// 	document.getElementById("matrixtime2").removeChild(matrixtime2.childNodes[0]);
-
-// 	var matrixp=document.getElementById("matrixtime2");
-
-// 	for (var i =0;i<=size1;i++){
-// 		matrixp.insertRow(i);
-// 		for(var j=0;j<=(size2-1);j++){
-// 			matrixp.rows[i].insertCell(j);
-// 		}
-// 	}
-// 	var matrix1=document.getElementById("matrixtime2").rows;
-
-// 	for (var i = 0 ; i < matrix1.length; i++) {
-
-// 		var column = matrix1[i].cells; 
-		
-// 		for (var j = 0; j < column.length ; j++) {
-
-// 			if (i>=2 && j===0){ 
-// 				for(var column in s1){
-// 					matrixtime2.rows[i].cells[j].innerHTML=s1[column];	
-// 					i++;	
-// 				}
-// 			}
-
-// 			if (i===0 && j>=2) {
-// 				for (var ligne in s2) {
-// 					matrixtime2.rows[i].cells[j].innerHTML=s2[ligne];
-// 					j++;
-
-// 				}
-// 			}
-// 			if(i>=1 && j===1){
-// 				for (path in matpath){
-// 					// matrixtime2.rows[i].cells[j].innerHTML=matpath[path];
-					
-// 					if (matpath[path]===0){
-// 						matrixtime2.rows[i].cells[j].setAttribute("id",i+","+j);
-// 						matrixtime2.rows[i].cells[j].innerHTML="<i class=\"fa fa-circle-thin\"></i>";
-// 						matrixtime2.rows[i].cells[j].style.visibility="hidden";
-// 					}
-// 					else if (matpath[path]===1){
-// 						matrixtime2.rows[i].cells[j].setAttribute("id",i+","+j);
-// 						matrixtime2.rows[i].cells[j].innerHTML="<i class=\"fa fa-arrow-left\"></i>";
-// 						matrixtime2.rows[i].cells[j].style.visibility="hidden";
-// 					}
-// 					else if (matpath[path]===2){
-// 						matrixtime2.rows[i].cells[j].setAttribute("id",i+","+j);
-// 						matrixtime2.rows[i].cells[j].innerHTML="<i class=\"fa fa-gavel\"></i>";
-// 						matrixtime2.rows[i].cells[j].style.visibility="hidden";
-// 					}
-// 					else if (matpath[path]===3){
-// 						matrixtime2.rows[i].cells[j].setAttribute("id",i+","+j);
-// 						matrixtime2.rows[i].cells[j].innerHTML="<i class=\"fa fa-arrow-up\"></i>";
-// 						matrixtime2.rows[i].cells[j].style.visibility="hidden";
-// 					}
-// 					j++;
-// 					if(j%size2==0){
-// 						i++;
-// 						j=1;
-// 					}
-// 				}
-// 				i=1;
-// 			}
-// 		}	
-// 	}	
-// document.getElementById("next").addEventListener("click",pass2);	
-// }	
-
-// function pass2(){
-
-// 	var matrix1=document.getElementById("matrixtime2").rows;
-
-// 	for (var i = 0 ; i < matrix1.length; i++) {
-
-// 		var column = matrix1[i].cells; 
-		
-// 		for (var j = 0; j < column.length ; j++) {
-
-// 			if (i===10 & j===10){
-// 				for(y in matrixtime){
-// 					document.getElementById(i+","+j).style.visibility="visible";
-// 					j--;
-// 					if(j%size2==0){
-// 						i--;
-// 						j=10;
-// 					}
-// 				}
-// 			}
-// 		}
-// 	}
-// }
